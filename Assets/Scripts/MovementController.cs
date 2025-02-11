e2b78776c0d813aef5082979cb543334fd432e26
Saptarshi Mallick <sapmallick@gmail.com>
2025-02-11 14:48:05 -0700

    Initial commit with completed part 1 and half done part 2

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,175 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MovementController : MonoBehaviour
+{
+    [Tooltip("The desired movement speed of character.")]
+    public float moveSpeed = 20.0f;
+
+    [Tooltip("Game Object that holds the Pathfinder data.")]
+    public Pathfinder pathfinder;
+
+    [Tooltip("Tile radius of the sword's area of effect attack.")]
+    public int areaEffectDist = 2;
+
+    [Tooltip("Recharge timer for the sword's area of effect attack.")]
+    public float rechargeTime = 5.0F;
+
+    private Animator animController;
+    private Camera   cam;
+
+    // start and end points of movement
+    private Vector3 _startPos;
+    private Vector3 _targetPos;
+    // linear interpolation parameter t
+    private float t = 0.0f;
+
+    private List<Pathfinder.PathNode> _movementPath;
+    private bool _isMoving;
+    private bool _canMagicAttack;
+
+    // co-routine method variable (pointer)
+    private IEnumerator coroutine;
+
+    private IEnumerator MoveToNext()
+    {
+        // direction vector (with magnitude)
+        Vector3 moveDir;
+
+        while (t < 1.0f)
+        {
+            moveDir = _targetPos - _startPos;
+
+            // find next interpolation step, enforcing constant
+            // a move rate to the target based on desired speed
+            t += Time.deltaTime * moveSpeed / moveDir.magnitude;
+
+            // transform between start position and end position
+            transform.position = Vector3.Lerp(_startPos, _targetPos, t);
+            
+            // pause execution for 1 tick of game loop
+            yield return null;
+        }
+
+        // finished with co-routine
+        this._isMoving = false;
+        yield return null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the animation controller on startup
+        this.animController = GetComponent<Animator>();
+
+        // find and save reference to scene camera
+        this.cam = (Camera) FindObjectOfType (typeof (Camera));
+
+        this._movementPath = new List<Pathfinder.PathNode>();
+        this._canMagicAttack = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left mouse button (on map): Move to location
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (this.coroutine != null)
+                StopCoroutine(this.coroutine);
+
+            Ray ray = this.cam.ScreenPointToRay (Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Vector3 start = this.transform.position;
+                Vector3 end = hit.point;
+
+                // Reset old path colors if there was one
+                if (this._movementPath.Count > 0)
+                    foreach (Pathfinder.PathNode n in this._movementPath)
+                        n.Deactivate();
+                
+                this._movementPath = this.pathfinder.FindPath(start, end);
+
+                if (this._movementPath.Count > 0)
+                {
+                    // Remove start node
+                    this._movementPath.RemoveAt(0);
+                    
+                    // Color path green
+                    foreach (Pathfinder.PathNode n in this._movementPath)
+                        n.Activate(Pathfinder.PathNode.PATH_COLOR);
+                    
+                    // Move
+                    this._isMoving = false;
+                    this.Advance();
+                }
+            }
+        }
+        else if(!this._isMoving && this.NeedsToMove()) // Continue movement path if the coroutine finishes
+            this.Advance();
+
+        // right mouse button or spacebar: Magical sword attack
+        if (this._canMagicAttack && (Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            animController.SetTrigger("Attack");
+
+            List<Pathfinder.PathNode> targetTiles =
+                this.pathfinder.FindAoE(this.transform.position, this.areaEffectDist);
+            
+            Debug.Log("Dijkstra's Algorithm: Found " + targetTiles.Count + " tiles in range.");
+            
+            foreach (Pathfinder.PathNode n in targetTiles)
+                n.Activate(Pathfinder.PathNode.AOE_COLOR);
+
+            StartCoroutine(ResetAoETileColors(targetTiles));
+
+            StartCoroutine(MagicAttackCooldown());
+        }
+    }
+
+    private IEnumerator MagicAttackCooldown()
+    {
+        yield return new WaitForSeconds(this.rechargeTime);
+        
+        this._canMagicAttack = true;
+        
+        yield return null;
+    }
+
+    private IEnumerator ResetAoETileColors(List<Pathfinder.PathNode> tiles)
+    {
+        yield return new WaitForSeconds(1.5F);
+        
+        foreach (Pathfinder.PathNode n in tiles.Where(n => n.GetColor() == Pathfinder.PathNode.AOE_COLOR)) 
+            n.Deactivate();
+
+        yield return null;
+    }
+
+    // Move to next spot along path (assumes a path does still exist)
+    private void Advance()
+    {
+        this._isMoving = true;
+        
+        this._startPos = this.transform.position;
+        this._targetPos = this._movementPath[0].transform.position;
+
+        this._movementPath[0].Deactivate();
+        this._movementPath.RemoveAt(0);
+
+        this._targetPos.y = this._startPos.y;
+        this.t = 0F;
+
+        this.coroutine = MoveToNext();
+        StartCoroutine(this.coroutine);
+    }
+
+    private bool NeedsToMove()
+    {
+        return this._movementPath.Count > 0;
+    }
+}
---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,255 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Pathfinder : MonoBehaviour
+{
+    public class PathNode
+    {
+        public static readonly Color PATH_COLOR = Color.green;
+        public static readonly Color AOE_COLOR = Color.cyan;
+        
+        public Transform transform;
+        public PathNode parent;
+        
+        public float gCost;
+        public float hCost;
+        public float fCost;
+
+        private readonly Color _default;
+
+        public PathNode(Transform transform)
+        {
+            this.transform = transform;
+            this._default = this.transform.GetComponent<MeshRenderer>().material.color;
+            this.Reset();
+        }
+
+        public void CalcF()
+        {
+            this.fCost = this.gCost + this.hCost;
+        }
+
+        public void Reset()
+        {
+            this.parent = null;
+            this.gCost = Mathf.Infinity;
+            this.hCost = 0F;
+            this.CalcF();
+        }
+
+        public void Activate(Color color)
+        {
+            this.transform.GetComponent<MeshRenderer>().material.color = color;
+        }
+
+        public void Deactivate()
+        {
+            this.transform.GetComponent<MeshRenderer>().material.color = this._default;
+        }
+
+        public Color GetColor()
+        {
+            return this.transform.GetComponent<MeshRenderer>().material.color;
+        }
+    }
+    
+    // Upper right and bottom left tiles of the labyrinth
+    public Transform upperRightTile;
+    public Transform bottomLeftTile;
+    
+    private PathNode[,] _labyrinth;
+    
+    private void Start()
+    {
+        // Dynamically load all walkable floor tiles and create a 2D array grid data structure
+        int xWidth = (int)((this.upperRightTile.position.x - this.bottomLeftTile.position.x) / 4 + 1);
+        int zWidth = (int)((this.upperRightTile.position.z - this.bottomLeftTile.position.z) / 4 + 1);
+        this._labyrinth = new PathNode[xWidth, zWidth];
+        
+        for (int i = 0; i < this._labyrinth.GetLength(0); i++)
+            for (int j = 0; j < this._labyrinth.GetLength(1); j++)
+                this._labyrinth[i, j] = null;
+        
+        foreach (Transform child in GetComponentsInChildren<Transform>())
+            if (child.gameObject.name.Contains("_Floor"))
+            {
+                (int x, int z) = GetIndex(child);
+                this._labyrinth[x, z] = new PathNode(child);
+            }
+    }
+
+    public List<PathNode> FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        List<PathNode> path = new List<PathNode>();
+        
+        // Convert input Vector3 into PathNodes
+        PathNode start = GetPathNode(startPos);
+        PathNode end = GetPathNode(targetPos);
+
+        if (start is null || end is null) return new List<PathNode>();
+        
+        // Reset any PathNode data to start fresh
+        for (int i = 0; i < this._labyrinth.GetLength(0); i++)
+            for (int j = 0; j < this._labyrinth.GetLength(1); j++)
+                if (this._labyrinth[i, j] is not null) this._labyrinth[i, j].Reset();
+        
+        // Setup A* variables
+        start.gCost = 0;
+        start.hCost = GetEuclideanDistance(start.transform, end.transform);
+        start.CalcF();
+
+        List<PathNode> openList = new List<PathNode>();
+        List<PathNode> closedList = new List<PathNode>();
+
+        PathNode current = start;
+        closedList.Add(current);
+
+        // A*
+        while (current != end)
+        {
+            foreach (PathNode n in this.GetAdjacentNodes(current))
+            {
+                if (closedList.Contains(n))
+                    continue;
+                else if (openList.Contains(n))
+                {
+                    float g = current.gCost + GetEuclideanDistance(current.transform, n.transform);
+                    if (g < n.gCost)
+                    {
+                        n.parent = current;
+                        n.gCost = g;
+                        n.CalcF();
+                    }
+                }
+                else
+                {
+                    n.parent = current;
+                    n.hCost = GetEuclideanDistance(n.transform, end.transform);
+                    n.gCost = current.gCost + GetEuclideanDistance(current.transform, n.transform);
+                    n.CalcF();
+                    openList.Add(n);
+                }
+            }
+
+            if (openList.Count == 0) break;
+
+            current = GetLowestFCostNode(openList);
+            openList.Remove(current);
+            closedList.Add(current);
+        }
+        
+        if (end.parent == null) return new List<PathNode>();
+        
+        // Calculate path from links
+        current = end;
+        while (current is not null)
+        {
+            path.Insert(0, current);
+            current = current.parent;
+        }
+        
+        return path;
+    }
+
+    public List<PathNode> FindAoE(Vector3 startPos, int maxSteps)
+    {
+        PathNode source = GetPathNode(startPos);
+        if (source == null) return new List<PathNode>();
+        
+        HashSet<PathNode> visited = new HashSet<PathNode>();
+        List<PathNode> queue = new List<PathNode>();
+        
+        // Reset any PathNode data to start fresh and add them to the queue
+        for (int i = 0; i < this._labyrinth.GetLength(0); i++)
+            for (int j = 0; j < this._labyrinth.GetLength(1); j++)
+                if (this._labyrinth[i, j] is not null)
+                {
+                    this._labyrinth[i, j].Reset();
+                    queue.Add(this._labyrinth[i, j]);
+                }
+        
+        // Dijkstra
+        while (queue.Count > 0)
+        {
+            // Find min distance node
+            PathNode min = GetLowestFCostNode(queue);
+
+            queue.Remove(min);
+            visited.Add(min);
+
+            foreach (PathNode v in this.GetAdjacentNodes(min))
+            {
+                float w = GetEuclideanDistance(min.transform, v.transform);
+                if (v.gCost > min.gCost + w)
+                {
+                    v.gCost = min.gCost + w;
+                    v.parent = min;
+                }
+            }
+        }
+        
+        // Prune a list of nodes that are within the step distance
+        return visited.Where(n => n.gCost <= maxSteps).ToList();
+    }
+    
+    private static PathNode GetLowestFCostNode(List<PathNode> list) 
+    {
+        PathNode min = list[0];
+        foreach (PathNode n in list)
+            if (n.fCost < min.fCost)
+                min = n;
+        return min;
+    }
+
+    private List<PathNode> GetAdjacentNodes(PathNode node)
+    {
+        List<PathNode> tiles = new();
+
+        (int x, int z) = GetIndex(node.transform);
+
+        AddTile(x + 1, z);
+        AddTile(x, z + 1);
+        AddTile(x - 1, z);
+        AddTile(x, z - 1);
+        // AddTile(x + 1, z + 1);
+        // AddTile(x - 1, z - 1);
+        // AddTile(x - 1, z + 1);
+        // AddTile(x + 1, z - 1);
+
+        return tiles;
+
+        void AddTile(int xc, int zc)
+        {
+            if (xc < 0 || xc >= this._labyrinth.GetLength(0) || zc < 0 || zc >= this._labyrinth.GetLength(1))
+                return;
+            
+            PathNode p = this._labyrinth[xc, zc];
+            if (p != null) tiles.Add(p);
+        }
+    }
+
+    private static float GetEuclideanDistance(Transform a, Transform b)
+    {
+        return Mathf.Sqrt(Mathf.Pow(a.position.x - b.position.x, 2) + Mathf.Pow(a.position.z - b.position.z, 2));
+    }
+
+    private PathNode GetPathNode(Vector3 pos)
+    {
+        pos += new Vector3(0, 1, 0);
+        if (!Physics.Raycast(pos, Vector3.down, out RaycastHit hit, 5)) 
+            return null;
+        
+        (int x, int z) = GetIndex(hit.transform);
+        return this._labyrinth[x, z];
+    }
+
+    private static (int, int) GetIndex(Transform tile)
+    {
+        return (((int)tile.position.x - 6) / 4, ((int)tile.position.z - 6) / 4);
+    }
+}
